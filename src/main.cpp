f6867673ad22f9e21ffcaa6dfae45b644be805be
Sam Title <samstitle@gmail.com>
2025-12-08 21:41:49 -0500

    feat: simple implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <sys/types.h>
+
+struct CracklePopData {
+    uint input;
+    std::string output;
+};
+
+auto cracklePop() -> void {
+    for (uint i = 1; i < 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "CracklePop ";
+        } else if (i % 3 == 0) {
+            std::cout << "Crackle ";
+        } else if (i % 5 == 0) {
+            std::cout << "Pop ";
+        } else {
+            std::cout << i << " ";
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!\n";
+    cracklePop();
+    std::cout << std::endl;
     return EXIT_SUCCESS;
 }
